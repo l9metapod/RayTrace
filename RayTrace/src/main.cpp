9bb41d06c914bcb063e6bb10332ab1b6c68c4b04
tianchen nan <138897811@qq.com>
2018-04-01 12:47:18 +0800

    新建项目，定义vec3

---
+++ RayTrace/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<fstream>
+
+using namespace std;
+
+int main() {
+	cout << "ray traceing initializing" << endl;
+	ofstream of("test.ppm");
+
+	int nx = 200;
+	int ny = 100;
+	of << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99*r);
+			int ig = int(255.99*g);
+			int ib = int(255.99*b);
+			of << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+	of.close();
+	cout << "\ntest end\n";
+	return 0;
+}
